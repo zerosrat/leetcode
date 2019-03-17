67d5b2e16a36d3880e1b377bcda062b0b271b633
zero <zerosrat@163.com>
2019-03-17 15:33:34 +0800

    6 & 8

---
+++ 006-zigzag-conversion/sortByRow.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+  numRows = Math.min(s.length, numRows)
+  if (numRows <= 1) {
+    return s
+  }
+
+  const arr = new Array(numRows)
+  for (let i = 0; i < numRows; i++) {
+    arr[i] = new Array(0)
+  }
+
+  for (let i = 0, j = 0, count = 0, goDown = true; count < s.length; count++) {
+    arr[i][j] = s.charAt(count)
+    if (goDown) {
+      i++
+    } else {
+      j++
+      i--
+    }
+    if ((count + 1) % (numRows - 1) === 0) {
+      goDown = !goDown
+    }
+  }
+
+  let result = ''
+  arr.forEach(row => {
+    row.forEach(c => {
+      if (typeof c !== 'undefined') {
+        result += c
+      }
+    })
+  })
+
+  return result
+}
