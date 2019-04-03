aa4d457bf651099257435eee2b55382d8af9e438
zero <zerosrat@163.com>
2019-04-03 11:21:37 +0800

    102,103

---
+++ 103-binary-tree-zigzag-level-order-traversal/queue.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+  const rtn = []
+  if (!root) return rtn
+  const queue = []
+  queue.unshift({
+    node: root,
+    index: 0
+  })
+  while (queue.length) {
+    const { node, index } = queue.pop()
+    if (!rtn[index]) {
+      rtn[index] = []
+    }
+    if (index % 2 === 0) {
+      rtn[index].push(node.val)
+    } else {
+      rtn[index].unshift(node.val)
+    }
+    if (node.left) {
+      queue.unshift({
+        node: node.left,
+        index: index + 1
+      })
+    }
+    if (node.right) {
+      queue.unshift({
+        node: node.right,
+        index: index + 1
+      })
+    }
+  }
+  return rtn
+}
