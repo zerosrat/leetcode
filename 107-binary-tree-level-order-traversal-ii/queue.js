9e875f516c176002f1821f2e4a43dd4ca285ee19
zero <zerosrat@163.com>
2019-04-03 20:46:46 +0800

    107

---
+++ 107-binary-tree-level-order-traversal-ii/queue.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+  const rtn = []
+  if (!root) return rtn
+  const queue = []
+  queue.unshift({
+    node: root,
+    index: 0
+  })
+  while (queue.length) {
+    const { node, index } = queue.pop()
+    if (!rtn[index]) {
+      rtn[index] = []
+    }
+    rtn[index].push(node.val)
+    if (node.left) {
+      queue.unshift({
+        node: node.left,
+        index: index + 1
+      })
+    }
+    if (node.right) {
+      queue.unshift({
+        node: node.right,
+        index: index + 1
+      })
+    }
+  }
+  return rtn.reverse()
+}
