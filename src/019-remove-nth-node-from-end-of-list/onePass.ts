14b38cd8357bdc52a5c3f4652359f3d6ec96cebb
zero <zerosrat@163.com>
2023-01-16 22:42:42 +0800

    refactor: 19 ts

---
+++ src/019-remove-nth-node-from-end-of-list/onePass.ts
@@ -0,0 +1,27 @@
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+  }
+}
+
+const removeNthFromEnd = function(head: ListNode, n: number) {
+  const dummy = new ListNode(0, head)
+  let left = dummy
+  let right = dummy
+
+  for (let i = 0; i < n; i++) {
+    right = right.next
+  }
+
+  while (right.next) {
+    left = left.next
+    right = right.next
+  }
+
+  left.next = left.next.next
+
+  return dummy.next
+}
